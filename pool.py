92c592b7367b4533ffd3b956daf3d4da8a380402
Vicky <victoria.guehennec@epita.fr>
2017-02-16 15:50:58 +0000

    Pool Add

--- pool.py
+++ pool.py
@@ -1,8 +1,24 @@
-class Server:
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
 
-    def _init_(self, int size, int capacity, int name, int pool):
-        self.size = size
-        self.capacity = capacity
+Ceci est un script temporaire.
+"""
+
+class Pool:
+    """
+    servers : liste des serveurs appartenant a cette pool
+    capacity : capacite totale des serveurs
+    """
+    def __init__(self, name, servers = [], capacity = 0):
         self.name = name
-        self.pool = pool
+        self.servers = servers
+        self.max_capacity = capacity
 
+def add_server(pool, server):
+    pool.servers.append(server)
+    pool.max_capacity += server.capacity
+
+def remove_server(pool, server):
+    pool.servers.remove(server)
+    pool.max_capacity -= server.capacity
