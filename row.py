afdccfe53b7adb4e82c5100a97a8d4061fc16f71
Hackerma <clement.fang@hotmail.fr>
2017-02-16 16:41:56 +0100

    row

---
+++ row.py
@@ -0,0 +1,17 @@
+class row:
+  def __init__(self, capacity):
+    self.capacity = capacity
+    self.data = [0] * capacity
+    self.servers = []
+
+def Print(self):
+  for i in range(capacity):
+    print(self.data[i])
+
+def addServer(self,server):
+  self.servers.append(server)
+
+def changeStatus(self, index, server)
+  self.data[îndex] = server
+
+
