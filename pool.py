8904d319527685fb3af378f1bc4c6b41170b07ba
Lionel <lionel.jaby@epita.fr>
2017-02-16 16:40:14 +0100

    du sale

---
+++ pool.py
@@ -0,0 +1,8 @@
+class Server:
+
+    def _init_(self, int size, int capacity, int name, int pool):
+        self.size = size
+        self.capacity = capacity
+        self.name = name
+        self.pool = pool
+
