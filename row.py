be69338452991ec4f64518cd695e2eac6d53ab21
Hackerma <clement.fang@hotmail.fr>
2017-02-16 16:55:22 +0100

    nb unable

--- row.py
+++ row.py
@@ -11,7 +11,19 @@ def Print(self):
 def addServer(self,server):
   self.servers.append(server)
 
-def changeStatus(self, index, server)
-  self.data[îndex] = server
+def changeStatus(self, index, server):
+  self.data[index] = server
 
+def nbUnable(self):
+  (cpt, nb) = (0, 0)
+  index = 0
+
+  for i in range(capacity):
+    if self.data[i] == 0:
+      cpt += 1
+    else:
+      if cpt > nb:
+        index = i
+        nb = cpt
+  return (nb, index)
 
