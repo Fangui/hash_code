2d77555ee575cadafadebd2396da07cd0821da23
Thomas Michelot <thomas.michelot@epita.fr>
2017-02-16 15:52:31 +0000

    add load

--- DT.py
+++ DT.py
@@ -6,15 +6,17 @@ Created on Thu Feb 16 16:22:57 2017
 """
 
 class DT:
-     
-     def _init_(self, rows, pools):
-         self.rows = rows;
-         self.pools = pools;
-         self.R,self.S,self.U,self.P,self.M = None;
-     def addrow (self, row):
+
+    def _init_(self, rows, pools):
+        self.rows = rows;
+        self.pools = pools;
+        self.R,self.S,self.U,self.P,self.M = None;
+    def addrow (self, row):
         self.rows.append(row);
-        
-     def addpool (self, pool):
+
+    def addpool (self, pool):
         self.pools.append(pool);
-         
-    
+
+    def load(self,path):
+        f = open(path)
+        self.R,self.S,self.U,self.P,self.M = f.readline().split(" ")
