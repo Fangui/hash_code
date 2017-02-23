2161425a631d952e8d10f22d9a12293c0145e766
Hackerma <clement.fang@hotmail.fr>
2017-02-23 21:12:10 +0100

    disp list

--- 2017/endPoint.py
+++ 2017/endPoint.py
@@ -10,12 +10,16 @@ class endPoint:
      self.list.append((cache,lat))
 
   def savingLat(self):
-    savings = [0] * len(self.list)
-    for i in range(len(self.list)):
+    Length = len(self.list)
+    savings = [0] * Length
+    for i in range(Length):
       latSaved = self.lat1 - self.list[i][1]
       savings[i] = (self.list[i][0], latSaved)
 
     sorted(savings, key = getKey)
     return savings
 
- 
+  def disp(List):
+    for i in range(len(List)):
+      print(List[i][1])
+
