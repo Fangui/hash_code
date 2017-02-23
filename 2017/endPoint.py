ccb46551e3f00aea6802c599becf06c61a434aa2
Hackerma <clement.fang@hotmail.fr>
2017-02-23 19:35:32 +0100

    list latency

--- 2017/endPoint.py
+++ 2017/endPoint.py
@@ -1,5 +1,6 @@
 class endPoint:
-  def __init__(self, lat1, lat2, listCache):
+  def __init__(self, lat1, lat2, listCache, listLat):
     self.lat1 = lat1
     self.lat2 = lat2
     self.listCache = listCache
+    self.listLat = listLat
